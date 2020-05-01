6df0bdf3eb64e4bee241deed064f9be16e7d4f9d
David LeGare <dlegare.1001@gmail.com>
2020-05-01 17:37:57 -0500

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
+//! Conditionally take a value out of an option.
+//!
+//! This crate adds a `take_if` extension method to [`Option`] which conditionally
+//! takes the value out of an option, leaving `None` in its place if the value was
+//! taken. The predicate function is only called if the option is `Some`, and
+//! receives a reference to the option's contents.
+//!
+//! If you don't need to take the value conditionally, i.e. you always need to take
+//! the value, use [`Option::take`] instead.
+//!
+//! # Examples
+//!
+//! ```
+//! use take_if::TakeIf;
+//!
+//! let mut maybe_greeting = Some("Hello, World!");
+//!
+//! if let Some(greeting) = maybe_greeting.take_if(|greeting| greeting.starts_with("Hello")) {
+//!     println!(r#"Greeting {:?} starts with "Hello""#, greeting);
+//! } else {
+//!     println!(r#"There was no greeting, or it didn't start with "Hello""#);
+//! }
+//! ```
+//!
+//! [`Option`]: https://doc.rust-lang.org/std/option/enum.Option.html
+//! [`Option::take`]: https://doc.rust-lang.org/std/option/enum.Option.html#method.take
+
+/// Extension trait for `Option<T>` that adds the `take_if` method.
+///
+/// See the [crate-level documentation](./index.html) for more information.
 pub trait TakeIf {
+    /// The type contained in the `Option`.
     type Inner;
 
+    /// Takes value out of the `Option` if `predicate` returns `true`.
+    ///
+    /// See the [crate-level documentation](./index.html) for more information.
     fn take_if<F: FnOnce(&Self::Inner) -> bool>(&mut self, predicate: F) -> Option<Self::Inner>;
 }
 
